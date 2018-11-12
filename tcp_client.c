1088f7b48fcf46c34a672d365afe49605817aac2
blandforrest <30292379+blandforrest@users.noreply.github.com>
2018-11-12 16:42:30 -0500

    Updated Client & Server
    Added struct to pass IP address and thread number to the run client function. Declared a global file variable that all three threads will use. Created an init message so the client will send the server it's writer number upon creation. At the moment, the server is not placing anything in shared memory, only printing the output of the data it has received.

--- tcp_client.c
+++ tcp_client.c
@@ -9,111 +9,158 @@
 #include <errno.h>
 #include <pthread.h>
 
+#define MAX  1000
+#define PORT_NUM 2008
 
-#define MAX  15
-#define PORT_NUM 7891
+//Argument struct for pthread_create
+struct arg_struct{
+	const char *arg1;
+	char   *arg2;
+};
 
-
+FILE *fp;
 
 void delay();
-void *run_client(void * in_arg);
+void *run_client(void * arguments);
 void create_threads(const char * ip_addr);
 void delete_thread(pthread_t threadArray[3]);
 
-int main(int argc, char **argv) {
-	if(argc != 2) {
-		printf("Usage: /{filename} {server_ip}\n");
-		return -1;
-	}
-	char * ip_addr = argv[1];
 
+
+
+int main(int argc, char **argv) {
+    
+    if(argc != 3) {
+        printf("Usage: /{filename} {server_ip}\n");
+        return -1;
+    }
+    char * ip_addr = argv[2];
+    
+	//Global FP
+	fp = fopen("input.txt", "r");
+	
 	create_threads(ip_addr);
+    
+
 	return 0;
 }
 
-
 void create_threads(const char * ip_addr){
-		//Initializing threads
-		pthread_t threadArray[3];
-		pthread_attr_t attr;
-		pthread_attr_init(&attr);
+	char * thread_ID = "1";
+	
+	struct arg_struct arguments;
+	arguments.arg1 = ip_addr;
+	arguments.arg2 = thread_ID;
 
-		//Creating three writer threads
-		int i;
-		for(i = 0; i < 3; i++){
-			if (pthread_create(&threadArray[i], NULL, run_client, (void *)ip_addr) != 0) {
-				perror("Unable to create thread to handle new connection");
-				exit(EXIT_FAILURE);
-			}
+	//Initializing threads
+    pthread_t threadArray[3];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    
+    //Creating three writer threads
+    int i;
+    for(i = 0; i < 3; i++){
+        if (pthread_create(&threadArray[i], NULL, run_client,(void *) &arguments) != 0){
+            perror("Unable to create thread to handle new connection");
+            exit(EXIT_FAILURE);
+        }
+    	arguments.arg2++;
 	}
-	delete_thread(threadArray);
+    delete_thread(threadArray);
 }
 
 void delete_thread(pthread_t threadArray[3]){
-	int i;
-	for(i = 0; i < 3; i++){
-		pthread_join(threadArray[i],NULL);
-	}
+    int i;
+    for(i = 0; i < 3; i++){
+        pthread_join(threadArray[i],NULL);
+    }
 }
+
+
 
 
 void delay(int number_of_seconds){
-	// Converting time into milli_seconds
-	int milli_seconds = 1000 * number_of_seconds;
-
-	// Stroing start time
-	clock_t start_time = clock();
-
-	// looping till required time is not acheived
-	while (clock() < start_time + milli_seconds);
+    // Converting time into milli_seconds
+    int milli_seconds = 1000 * number_of_seconds;
+    
+    // Stroing start time
+    clock_t start_time = clock();
+    
+    // looping till required time is not acheived
+    while (clock() < start_time + milli_seconds);
 }
 
 
+//Takes IP address and ID of writer thread
+void *run_client(void *arguments){
+    char received[MAX], to_send[MAX];
+    int net_socket, conn_stat, valread;
+    struct sockaddr_in server_address;
+    socklen_t  address_size;
+    char word[15];
+  	char init_mssg[20] = "Writer #";
 
-void *run_client(void * in_arg){
-	const char * ip_addr  = (const char *)in_arg;
-	char received[MAX], to_send[MAX], username[MAX];
-	int net_socket, conn_stat, valread;
-	struct sockaddr_in server_address;
-	socklen_t address_size;
+	struct arg_struct *args = arguments;
+ 
+	const char *ip_addr = args->arg1;
+	char * thread_ID = args->arg2;
 
-	FILE * fp = fopen("mydata.txt", "r");
+	strcat(init_mssg, thread_ID); 
 
-	if((net_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-		perror("Socket Failed");
-		exit(EXIT_FAILURE);
-	}
-
-	//Server address is IPv4 and port is PORT_NUM
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(PORT_NUM);
-	server_address.sin_addr.s_addr = inet_addr(ip_addr);
-
-	// address_size = sizeof server_address;
-	if(connect(net_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1){
-		printf("There was an error connecting to the socket\n");
-		printf("ERRNO %s\n",strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-
-
-	// TODO - Shared Buffer needs to be created
-	memset(to_send, 0, sizeof(to_send));
-	// fgets(shared_buffer, MAX, fp);
-
-
-	//Sending message to server
-	if(send(net_socket, to_send, strlen(to_send), 0) < 0) {
-		printf("Error: send() failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	// Receive reply and terminate
-	if(recv(net_socket, received, sizeof(received), 0) < 0) {
-		printf("Error: recv() failed\n");
-		exit(EXIT_FAILURE);
-	}
-	printf("Server: %s\n", received);
-
-	return;
+    if((net_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("Socket Failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    //Server address is IPv4 and port is PORT_NUM
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT_NUM);
+    server_address.sin_addr.s_addr = inet_addr(ip_addr);
+    
+    // address_size = sizeof server_address;
+    if(connect(net_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1){
+        printf("There was an error connecting to the socket\n");
+        printf("ERRNO %s\n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    
+    // Receiving welcome message
+    /*recv(net_socket, received, sizeof(received),0);
+    printf("%s", received);
+    memset(received, 0, sizeof(received));
+    */
+    
+    if(fp == NULL)
+    {
+		printf("File Open Error\n");
+        exit(EXIT_FAILURE);
+    }
+    fgets(word,15,fp);
+    
+   /* if(send(net_socket,word,strlen(word),0) < 0)
+    {
+        printf("Error: send() failed\n");
+        exit(EXIT_FAILURE);
+    }*/
+    
+    // Input message
+    /*printf("%s: ", username);
+    memset(to_send, 0, sizeof(to_send));
+    fgets(to_send, MAX, stdin);
+    printf("\n");*/
+    
+    //Sending message to server
+    if(send(net_socket, init_mssg, strlen(init_mssg), 0) < 0) {
+        printf("Error: send() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Receive reply and terminate
+    if(recv(net_socket, received, sizeof(received), 0) < 0) {
+        printf("Error: recv() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("Server: %s\n", received);
+    
 }
+           
--- tcp_server.c
+++ tcp_server.c
@@ -13,11 +13,13 @@
 #include <netinet/in.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <pthread.h> 
+#include <pthread.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 
 #define MAX 1000
-#define PORT_NUM 7891
+#define PORT_NUM 2008
 
 
 void create_server();
@@ -26,89 +28,114 @@ void *messager(void *in_arg);
 
 
 int main(){
-	create_server();
-	return 0;
+    create_server();
+    return 0;
 }
 
 
 
 void create_server(){
-
+    
     int server_fd, new_socket;
    	struct sockaddr_storage server_storage;
-	socklen_t addr_size;
-	struct sockaddr_in address;
+    socklen_t addr_size;
+    struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-	pthread_t thread_id;
-   
-	
-	// Creating socket file descriptor
-	//Socket is using IPv4 and TCP. Checks if NULL
+    pthread_t thread_id;
+    
+    
+    // Creating socket file descriptor
+    //Socket is using IPv4 and TCP. Checks if NULL
     if ((server_fd = socket(PF_INET, SOCK_STREAM, 0)) == -1){
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-
-	//Address is a struct of sockaddr_in and uses IPv4 and the localhost for IP
-    address.sin_family = AF_INET; 
+    
+    //Address is a struct of sockaddr_in and uses IPv4 and the localhost for IP
+    address.sin_family = AF_INET;
     address.sin_port = htons(PORT_NUM);
-	address.sin_addr.s_addr = htonl(INADDR_ANY);
-
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    
     // Forcefully attaching socket to the port PORT_NUM
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0){
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-	printf("Listening...\n");
+    printf("Listening...\n");
     if (listen(server_fd, 3) < 0){
         perror("listen");
         exit(EXIT_FAILURE);
     }
-
-	int remaining_connections = 3; // TODO: change while condition to check shared memory for notification to terminate? 
-	while (remaining_connections > 0)  {
-		addr_size = sizeof(server_storage);
-		if ((new_socket = accept(server_fd, (struct sockaddr *) &server_storage, &addr_size)) < 0){
-				perror("accept");
-				exit(EXIT_FAILURE);
-		}
-		if (pthread_create(&thread_id, NULL, messager, (void *)(intptr_t)new_socket) != 0) {
-			perror("Unable to create thread to handle new connection");
-			exit(EXIT_FAILURE);
-		}
-		remaining_connections--;
-	}
+    
+    int remaining_connections = 3; // TODO: change while condition to check shared memory for notification to terminate?
+    while (remaining_connections > 0)  {
+        addr_size = sizeof(server_storage);
+        if ((new_socket = accept(server_fd, (struct sockaddr *) &server_storage, &addr_size)) < 0){
+            perror("accept");
+            exit(EXIT_FAILURE);
+            
+        }
+        if (pthread_create(&thread_id, NULL, messager, (void *)(intptr_t)new_socket) != 0) {
+            perror("Unable to create thread to handle new connection");
+            exit(EXIT_FAILURE);
+            
+        }
+        remaining_connections--;
+    }
 }
 
 
 
 void *messager(void *in_arg){
-	int new_socket = (intptr_t) in_arg;
+    
+    int new_socket = (intptr_t) in_arg;
     char buffer[MAX] = {0};
-	char *message = "testing, testing, 1, 2, 3..."; // Test response
-	char hello[] = 
-"\n   *****************************\n\
-   *                           *\n\
-   *       Welcome to the      *\n\
-   *           server!         *\n\
-   *                           *\n\
-   *                           *\n\
-   *****************************\n\n\n\n\n";
- 
-	// Send server greeting message
-	send(new_socket, hello, strlen(hello),0);
-
-	// Receive writer message
-	recv(new_socket, buffer, 1024,0);
+    char *message = "testing, testing, 1, 2, 3..."; // Test response
+    char hello[] =
+    "\n   *****************************\n\
+    *                           *\n\
+    *       Welcome to the      *\n\
+    *           server!         *\n\
+    *                           *\n\
+    *                           *\n\
+    *****************************\n\n\n\n\n";
+    //create shared memory str
+    
+    key_t key = ftok("shmfile",65);
+    int shmid = shmget(key, 1024, 0666|IPC_CREAT);
+    char *str = (char*) shmat(shmid,(void*)0,0);
+    
+    
+    // Send server greeting message
+   // send(new_socket, hello, strlen(hello),0);
+    
+    // Receive writer message
 	
-	printf("Other user: %s", buffer);
 
-	sleep(2);
+    recv(new_socket, buffer, 1024,0);
+    printf("%s", buffer);
+	recv(new_socket,buffer, 1024,0);
+	printf("%s",buffer);
 
-	// Send whatever is in shared memory
-	send(new_socket, message, strlen(message), 0);
 
-	close(new_socket); 
-	pthread_exit(NULL);
+	int i = 0;
+    while (buffer[i] != '\0')
+    {
+        str[i] = buffer[i];
+	    buffer[i]; 
+   		i+=1;
+    }
+    printf("%s", str);
+    
+    sleep(2);
+    
+    // Send whatever is in shared memory
+    send(new_socket, str, strlen(str), 0);
+    
+    shmdt(str);
+    shmctl(shmid,IPC_RMID,NULL);
+    
+    close(new_socket); 
+    pthread_exit(NULL);
 }
